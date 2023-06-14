7f9601882d2f42f22052d1922d552adc623b2cb4
Pablo Vicente <pablovicente19@hotmail.com>
2023-06-14 20:35:27 -0300

    terminar la seccion de proyectos

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,21 @@
 const $menuBurguer = document.querySelector(".bars-menu-content");
 const $menuList = document.querySelector(".navbar-list");
-const $iconExit = document.querySelector(".icon-exit-content");
+const $iconExitMenu = document.querySelector(".icon-exit");
 
-$menuBurguer.addEventListener("click", () => {
-	$menuList.classList.toggle("show-menu");
+// Cuando se hace click al menu hamburguesa se habre
+const showMenu = () => {
+  $menuList.classList.toggle("show-menu");
 	$menuBurguer.classList.toggle("hidden");
-	$iconExit.classList.toggle("show");
-});
+  $iconExitMenu.classList.toggle("show");
+}
+// Evento que cuando se hace click se cierra el menu
+const closeMenu = () => {
+  $menuList.classList.toggle("show-menu");
+	$menuBurguer.classList.toggle("hidden");
+  $iconExitMenu.classList.toggle("show");
+}
+const init = () => {
+  $menuBurguer.addEventListener('click',showMenu);
+  $iconExitMenu.addEventListener('click', closeMenu)
+}
+init()
