e3a60fb886a4ea2e92c2e0166482aa70eedd36e9
Pablo Vicente <pablovicente19@hotmail.com>
2023-06-14 14:08:34 -0300

    terminar el menu ha,gurgesa

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 const $menuBurguer = document.querySelector(".bars-menu-content");
 const $menuList = document.querySelector(".navbar-list");
+const $iconExit = document.querySelector(".icon-exit-content");
+
 $menuBurguer.addEventListener("click", () => {
 	$menuList.classList.toggle("show-menu");
+	$menuBurguer.classList.toggle("hidden");
+	$iconExit.classList.toggle("show");
 });
