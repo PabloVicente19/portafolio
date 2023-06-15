f4bafa6c65edbcb5d6bdd38433c0d50231c4341a
Pablo Vicente <pablovicente19@hotmail.com>
2023-06-15 14:04:11 -0300

    seguis con el diseño

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,20 @@ const $iconExitMenu = document.querySelector(".icon-exit");
 
 // Cuando se hace click al menu hamburguesa se habre
 const showMenu = () => {
-  $menuList.classList.toggle("show-menu");
+	$menuList.classList.toggle("show-menu");
 	$menuBurguer.classList.toggle("hidden");
-  $iconExitMenu.classList.toggle("show");
-}
+	$iconExitMenu.classList.toggle("show");
+};
 // Evento que cuando se hace click se cierra el menu
 const closeMenu = () => {
-  $menuList.classList.toggle("show-menu");
+	$menuList.classList.toggle("show-menu");
 	$menuBurguer.classList.toggle("hidden");
-  $iconExitMenu.classList.toggle("show");
-}
+	$iconExitMenu.classList.toggle("show");
+};
+
 const init = () => {
-  $menuBurguer.addEventListener('click',showMenu);
-  $iconExitMenu.addEventListener('click', closeMenu)
-}
-init()
+	$menuBurguer.addEventListener("click", showMenu);
+	$iconExitMenu.addEventListener("click", closeMenu);
+};
+
+init();
