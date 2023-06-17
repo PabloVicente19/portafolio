514f92e0aecffb68ddd3ff077434c680864b5121
Pablo Vicente <pablovicente19@hotmail.com>
2023-06-17 09:50:50 -0300

    Renderize las cards

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const $menuBurguer = document.querySelector(".bars-menu-content");
 const $menuList = document.querySelector(".navbar-list");
 const $iconExitMenu = document.querySelector(".icon-exit");
+const $projectsContainer = document.getElementById('projects')
 
 // Cuando se hace click al menu hamburguesa se habre
 const showMenu = () => {
@@ -15,9 +16,30 @@ const closeMenu = () => {
 	$iconExitMenu.classList.toggle("show");
 };
 
+
+// Creo las cards de los proyectos
+const createCard = (arr) => {
+  const {name, image, link} = arr;
+  return `
+  <div class="projets-content">
+    <img src=${image} alt=${name} class="projets-img">
+   <div class="projets-text">
+      <a href=${link} target="_blank">Ver Proyecto</a>
+    </div>
+  </div>
+  `
+}
+// Renderizo las cards en el contenedo html
+const renderCards = (container) => {
+  return container.innerHTML = projects.map(createCard).join("")
+}
+
+
+
 const init = () => {
 	$menuBurguer.addEventListener("click", showMenu);
 	$iconExitMenu.addEventListener("click", closeMenu);
+  renderCards($projectsContainer);
 };
 
 init();
