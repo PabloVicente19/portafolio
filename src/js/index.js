ac9d61dc091185f296ef20d98f2aa785f326e1f9
Pablo Vicente <pablovicente19@hotmail.com>
2023-06-26 14:49:19 -0300

    renderizar las imagenes

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,35 @@
-const $menuBurguer = document.querySelector(".bars-menu-content");
+const $menuBurger = document.querySelector(".bars-menu-content");
 const $menuList = document.querySelector(".navbar-list");
-const $iconExitMenu = document.querySelector(".icon-exit");
-const $projectsContainer = document.getElementById('projects')
+const $iconCloseMenu = document.querySelector(".icon-exit");
+const $projectsContainer = document.querySelector(".projects-container");
 
-// Cuando se hace click al menu hamburguesa se habre
-const showMenu = () => {
+$menuBurger.addEventListener("click", (e) => {
 	$menuList.classList.toggle("show-menu");
-	$menuBurguer.classList.toggle("hidden");
-	$iconExitMenu.classList.toggle("show");
-};
-// Evento que cuando se hace click se cierra el menu
-const closeMenu = () => {
-	$menuList.classList.toggle("show-menu");
-	$menuBurguer.classList.toggle("hidden");
-	$iconExitMenu.classList.toggle("show");
-};
-
+});
+$iconCloseMenu.addEventListener("click", (e) => {
+	$menuList.classList.remove("show-menu");
+});
 
 // Creo las cards de los proyectos
 const createCard = (arr) => {
-  const {name, image, link} = arr;
-  return `
-  <div class="projets-content">
-    <img src=${image} alt=${name} class="projets-img">
+	const { name, image, link } = arr;
+	//<img src=${image} alt=${name} class="projets-img">
+	return `
+	
+  <div class="project-content" style=background-color:"white";>
    <div class="projets-text">
       <a href=${link} target="_blank">Ver Proyecto</a>
     </div>
   </div>
-  `
-}
+  `;
+};
 // Renderizo las cards en el contenedo html
 const renderCards = (container) => {
-  return container.innerHTML = projects.map(createCard).join("")
-}
-
-
+	return (container.innerHTML = projects.map(createCard).join(""));
+};
 
 const init = () => {
-	$menuBurguer.addEventListener("click", showMenu);
-	$iconExitMenu.addEventListener("click", closeMenu);
-  renderCards($projectsContainer);
+	renderCards($projectsContainer);
 };
 
 init();
